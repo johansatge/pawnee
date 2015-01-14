16cb8be11efedb78877f06f2749d98fde38fc588
Johan <contact@johansatge.fr>
2015-01-14 20:06:01 +0100

    Updates Gruntfile.js to nwjs

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,41 +16,10 @@ module.exports = function(grunt)
     {
         setDevMode(grunt.option('dev') === true);
         var done = this.async();
-        var child = exec('/Applications/node-webkit.app/Contents/MacOS/node-webkit ./app');
+        var child = exec('/Applications/nwjs.app/Contents/MacOS/nwjs ./app');
         child.stdout.on('data', grunt.log.write);
         child.stderr.on('data', grunt.log.write);
         child.on('close', done);
-    });
-
-    /**
-     * Builds the app
-     */
-    grunt.registerTask('build', function()
-    {
-        var builder = new nwb(
-            {
-                files: './app/**/**',
-                platforms: ['osx'],
-                buildDir: './build',
-                macCredits: false,
-                macIcns: './assets/icns/icon.icns'
-            });
-        var done = this.async();
-        grunt.log.writeln('Building app...');
-        setDevMode(false);
-        builder.build().then(function()
-        {
-            grunt.log.ok('Build done.');
-            if (grunt.option('launch'))
-            {
-                exec('./build/Pawnee/osx/Pawnee.app/Contents/MacOS/node-webkit');
-            }
-            done();
-        }).catch(function(error)
-        {
-            grunt.log.error(error);
-            done();
-        });
     });
 
     /**
