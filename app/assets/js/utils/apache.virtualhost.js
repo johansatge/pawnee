09811e0a16e3b7cc496f69c43d58f8cc8d32d698
Johan <contact@johansatge.fr>
2015-01-26 21:27:42 +0100

    Gets virtual hosts and builds the list

--- app/assets/js/utils/apache.virtualhost.js
+++ app/assets/js/utils/apache.virtualhost.js
@@ -18,65 +18,64 @@
         app.node.exec('cat ' + app.models.apache.confPath, function(error, stdout, stderr)
         {
             app.logActivity(stderr);
-
-
-            var lines = stdout.split('\n');
-            var raw_vhosts = [];
-            var current_vhost = false;
-            for(var index = 0; index < lines.length; index += 1)
+            var raw_vhosts = _parseConfigurationFile(stdout);
+            var vhosts = [];
+            for (var index = 0; index < raw_vhosts.length; index += 1)
             {
-                if (lines[index].search(/ *?<VirtualHost/i) !== -1)
-                {
-                    current_vhost = raw_vhosts.length;
-                    raw_vhosts[current_vhost] = '';
-                }
-                if (current_vhost !== false)
-                {
-                    raw_vhosts[current_vhost] += lines[index] + '\n';
-                }
-                if (lines[index].search(/ *?<\/VirtualHost>/i) !== -1)
-                {
-                    current_vhost = false;
-                }
+                vhosts.push(_parseVirtualHost(raw_vhosts[index]));
             }
-
-            app.log(raw_vhosts);
-
-
-            // @todo
-            // /DocumentRoot[ "]+([^"]+)"/i
-            // /ServerName[ "]+([^"]+)"/i
-            // /<VirtualHost +([^:]+):([^>]+)>/i
-
-
-
-            callback([{name: '@todo list'}]);
+            callback(vhosts);
         });
     };
 
     /**
-     * Gets the list of enabled modules
-     * @param available_modules
-     * @param callback
-     *
-    var _checkEnabledModules = function(available_modules, callback)
+     * Parses the given virtual host
+     * @param raw_vhost
+     * @returns object
+     */
+    var _parseVirtualHost = function(raw_vhost)
     {
-        app.node.exec('cat ' + app.models.apache.confPath, function(error, stdout, stderr)
+        var vhost = {};
+        vhost.raw = raw_vhost;
+        var document_root = new RegExp(/DocumentRoot[ "]+([^";\n]+)/i).exec(raw_vhost);
+        vhost.document_root = document_root !== null && typeof document_root[1] !== 'undefined' ? document_root[1] : '';
+        var server_name = new RegExp(/ServerName[ "]+([^";\n]+)/i).exec(raw_vhost);
+        vhost.server_name = server_name !== null && typeof server_name[1] !== 'undefined' ? server_name[1] : '';
+        var ip = new RegExp(/<VirtualHost +([^:]+):([^>]+)>/i).exec(raw_vhost);
+        vhost.ip = ip !== null && typeof ip[1] !== 'undefined' ? ip[1] : '';
+        vhost.port = ip !== null && typeof ip[2] !== 'undefined' ? ip[2] : '';
+        vhost.name = vhost.server_name !== '' ? vhost.server_name : '--';
+        return vhost;
+    };
+
+    /**
+     * Looks for vhosts in httpd.conf
+     * @param httpd
+     * @returns Array
+     */
+    var _parseConfigurationFile = function(httpd)
+    {
+        var lines = httpd.split('\n');
+        var raw_vhosts = [];
+        var current_vhost = false;
+        for (var index = 0; index < lines.length; index += 1)
         {
-            app.logActivity(stderr);
-            var enabled_modules = [];
-            app.utils.regexp.search(/[^#]?LoadModule\s(.*)_module.*\.so/gi, stdout, function(match)
+            if (lines[index].search(/ *?<VirtualHost/i) !== -1)
             {
-                enabled_modules.push(match);
-            });
-            var modules = [];
-            app.utils.regexp.search(/mod_([^.]*)\.so/g, available_modules, function(match)
+                current_vhost = raw_vhosts.length;
+                raw_vhosts[current_vhost] = '';
+            }
+            if (current_vhost !== false)
             {
-                modules.push({name: match, filename: match + '.so', enabled: enabled_modules.indexOf(match) !== -1});
-            });
-            callback(modules);
-        });
-    };*/
+                raw_vhosts[current_vhost] += lines[index] + '\n';
+            }
+            if (lines[index].search(/ *?<\/VirtualHost>/i) !== -1)
+            {
+                current_vhost = false;
+            }
+        }
+        return raw_vhosts;
+    };
 
     app.utils.apache.virtualhost = module;
 
